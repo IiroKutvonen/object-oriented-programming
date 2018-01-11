203841841f2b03c5b8185b1d3e1e54a4e0c348cd
Iiro Kutvonen <kutvonen.iiro@gmail.com>
2018-01-11 10:43:52 +0200

    TaskPerson 0.1

---
+++ object-method/TaskPerson/TaskPerson/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskPerson
+{
+    class Person
+    {
+        //Fields
+        public string Name;
+        private int age;
+
+        //Constructor
+        public Person()
+        {
+            Name = "unknown";
+            age = 0;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            this.age = age;
+        }
+
+
+        //Methods
+        public void GrowOld()
+        {
+            this.age++;
+        }
+
+        public void PrintPersonInfo()
+        {
+            Console.WriteLine($"Nimi: {Name}\nIkä: { this.age}");
+        }
+
+    }
+}
